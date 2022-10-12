7e87254658e0ca9f0521fbd125c92f0e80aac293
ozen0416 <enzoleewood1gmail.com>
2022-10-12 17:17:52 +0200

    hangman

--- HangMan2.0/main.go
+++ HangMan2.0/main.go
@@ -1,5 +1,71 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
 func main() {
 
+	content, err := os.Open("words.txt")
+
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("An error has occur")
+	}
+	scanner := bufio.NewScanner(content)
+	scanner.Split(bufio.ScanWords)
+	tmp := make([]string, 0)
+	for scanner.Scan() {
+		tmp = append(tmp, scanner.Text())
+	}
+	found := []string{}
+	for i := 0; i < len(tmp); i++ {
+		found = append(found, "_")
+	}
+	Guesses := 10
+	for Guesses > 0 {
+		fmt.Println("Hello! All the words you need to find are in english :)")
+		fmt.Println("You have", Guesses, "remaining guesses")
+
+	}
+
+	rand.Seed(time.Now().Unix())
+	result := rand.Intn(len(tmp) - 1)
+
+	fmt.Println(tmp[result])
+}
+
+func GetLetter(found string) (string, error) {
+	letter := ""
+	var p rune
+	if p >= 65 || p <= 90 && p >= 97 || p <= 122 {
+		for _, a := range found {
+			tmp := a
+			if a >= 97 && a <= 122 {
+				tmp = a - 32
+			}
+			letter += string(tmp)
+		}
+
+	} else {
+		fmt.Println("This is not a letter !")
+	}
+	return letter, nil
+}
+
+func Join(strings []string, separator string) string {
+	if len(strings) == 0 {
+		return ""
+	}
+	s := "_"
+	index := len(strings) - 1
+	for _, v := range strings[:index] {
+		s += string(v) + separator
+	}
+	return s + strings[index]
 }
